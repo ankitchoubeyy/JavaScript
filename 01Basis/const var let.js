a06cb24807844b8ed4959609ee956907f2d73466
Ankit Choubey <imkitchoubey@gmail.com>
2023-10-28 16:24:23 +0530

    learned about js var,const and let keywords

---
+++ 01Basis/const var let.js
@@ -0,0 +1,10 @@
+console.log("Use of const,let and var keywords");
+
+const userId = 12345;
+let userEmail = "ankit@gmail.com";
+var userPhone = "+912345678"; // Note -->> Do not use var for declaring the variable beacuse it has the problems with block level and functional level.
+
+// -------->> Always try to use const and let for declaring the variables
+
+
+
