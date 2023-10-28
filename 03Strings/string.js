0eafb18a1a8af7e74dda809a1fb6699052784fa6
Ankit Choubey <imkitchoubey@gmail.com>
2023-10-28 21:00:25 +0530

    learn about strings in js

---
+++ 03Strings/string.js
@@ -0,0 +1,11 @@
+let myName = `Ankit`
+let marks = 50;
+
+// Modern way to use String is ``
+
+console.log(`My name is ${myName} and my marks is ${marks}`);
+
+console.log(myName.length);
+
+// Use MDN docs for more strings methods
+
