ff75f712bde78301374fad03e138f6e7f48cc42e
Ankit Choubey <imkitchoubey@gmail.com>
2023-11-01 21:52:07 +0530

    add forEachLoop

---
+++ 11Loops/forEachLoop.js
@@ -0,0 +1,12 @@
+let values = ["c++", "c", "Java", "Python"];
+
+values.forEach((item) => {
+  console.log(item);
+});
+
+/* Note: 
+
+1. forEach(()=>{}) --->> forEach contains the callback functions.
+2. It does not return any kind of value.
+
+*/
