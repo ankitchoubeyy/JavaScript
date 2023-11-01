26473bfa2f1c788333f48da02c047b7c332f0b90
Ankit Choubey <imkitchoubey@gmail.com>
2023-11-01 21:50:01 +0530

    add while loop

---
+++ 11Loops/whileLoop.js
@@ -0,0 +1,26 @@
+// --------- While Loop ------------
+let i = 0;
+while (i <= 15) {
+  console.log(i);
+  i += 2;
+}
+
+// another example with an array
+
+let myArray = [12, 34, 56, 78];
+let arr = 0;
+while (arr < myArray.length) {
+  console.log(myArray[arr]);
+  arr++;
+}
+
+// ---------- do while loop -----------------
+let score = 1;
+do{
+  console.log(`Score is ${score}`);
+  score++;
+}while(score <= 20);
+
+// do while : kaam pahle condition baad me.
+
+
